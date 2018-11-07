054298a8eae3ceb6fce0421d3f87d38b235d4b53
Sehajpreetsingh <kand@usc.edu>
2018-11-07 12:39:19 -0800

    Profile
    Bio and Pic working

--- app/src/main/java/com/example/kindler/Profile.java
+++ app/src/main/java/com/example/kindler/Profile.java
@@ -1,38 +1,191 @@
 package com.example.kindler;
 
-import android.arch.lifecycle.ViewModelProviders;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.ImageView;
 
-import Database.UserViewModel;
+public class Profile extends AppCompatActivity implements View.OnClickListener{
 
-public class Profile extends AppCompatActivity {
-
-    private UserViewModel mUserViewModel;
+    private TextView name;
+    private ImageView profilePicture;
+    private TextView biography;
+    Bitmap bitmap;
+    EditText edtBio,edtName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        mUserViewModel = ViewModelProviders.of(this).get(UserViewModel.class);
 
-        ImageView profilePicture = findViewById(R.id.profilePicture);
-        TextView name = findViewById(R.id.name);
-        TextView biography = findViewById(R.id.biography);
-        /*
-        profilePicture.setImageResource(R.drawable.test);
-        name.setText("S");
-        biography.setText("Something");
-        */
+        init();
+        listeners();
 
-        profilePicture.setImageResource(R.drawable.test);
-        name.setText(mUserViewModel.getCurrProfile().getProfileName());
-        biography.setText(mUserViewModel.getCurrProfile().getProfileBiography());
-
+//        UserViewModel mUserViewModel = ViewModelProviders.of(this).get(UserViewModel.class);
+//
+//        mUserViewModel.getCurrUser().observe(this, new Observer<User>() {
+//            @Override
+//            public void onChanged(@Nullable User user) {
+//                name.setText(user.getProfile().getProfileName());
+//            }
+//        });
 
     }
 
+    public void init(){
 
+        profilePicture = findViewById(R.id.profilePicture);
+        name = findViewById(R.id.name);
+        biography = findViewById(R.id.biography);
+        edtBio= findViewById(R.id.edtBiography);
+        edtName=findViewById(R.id.edtName);
+
+        profilePicture.setImageResource(R.drawable.test);
+        name.setText("Samantha Chang");
+        biography.setText("Student studying CS at USC");
+
+    }
+
+    public void listeners(){
+
+        profilePicture.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()){
+            case R.id.profilePicture:
+                CameraDialog cameraDialog=new CameraDialog(Profile.this);
+                cameraDialog.show();
+
+
+
+                break;
+
+            default:
+                break;
+
+        }
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (resultCode == RESULT_OK) {
+
+            if (requestCode == 1) {
+                bitmap = (Bitmap) data.getExtras().get("data");
+                bitmap = Bitmap.createScaledBitmap(bitmap, 200, 200, true);
+
+                profilePicture.setImageBitmap(bitmap);
+
+             //   strProfile=encodeToBase64(bitmap, Bitmap.CompressFormat.JPEG, 100);
+
+            } else if (requestCode == 2) {
+                try {
+                    Uri selectedImage = data.getData();
+                    String[] filePath = {MediaStore.Images.Media.DATA};
+                    Cursor c = getApplicationContext().getContentResolver().query(selectedImage, filePath, null, null, null);
+                    c.moveToFirst();
+                    int columnIndex = c.getColumnIndex(filePath[0]);
+                    String picturePath = c.getString(columnIndex);
+                    c.close();
+                    bitmap = (BitmapFactory.decodeFile(picturePath));
+                    bitmap = Bitmap.createScaledBitmap(bitmap, 200, 200, true);
+                    profilePicture.setImageBitmap(bitmap);
+
+              //      strProfile=encodeToBase64(bitmap, Bitmap.CompressFormat.JPEG, 100);
+
+                } catch (Exception e) {
+                    Log.e("Exception",e.toString());
+                }
+            }
+//            else if (requestCode==3){
+//                bitmap = (Bitmap) data.getExtras().get("data");
+//                bitmap = Bitmap.createScaledBitmap(bitmap, 200, 200, true);
+//
+//                bgImageView.setImageBitmap(bitmap);
+//
+//               // strBg=encodeToBase64(bitmap, Bitmap.CompressFormat.JPEG, 100);
+//            }
+//            else if (requestCode==4){
+//                try {
+//                    Uri selectedImage = data.getData();
+//                    String[] filePath = {MediaStore.Images.Media.DATA};
+//                    Cursor c = context.getContentResolver().query(selectedImage, filePath, null, null, null);
+//                    c.moveToFirst();
+//                    int columnIndex = c.getColumnIndex(filePath[0]);
+//                    String picturePath = c.getString(columnIndex);
+//                    c.close();
+//                    bitmap = (BitmapFactory.decodeFile(picturePath));
+//                    bitmap = Bitmap.createScaledBitmap(bitmap, 200, 200, true);
+//                    bgImageView.setImageBitmap(bitmap);
+//
+//                    strBg=encodeToBase64(bitmap, Bitmap.CompressFormat.JPEG, 100);
+//
+//                } catch (Exception e) {
+//                    Log.e("Exception",e.toString());
+//                }
+//            }
+        }
+    }
+
+    public class CameraDialog extends Dialog{
+
+        Context context;
+        TextView camera,gallery,cancel;
+
+        public CameraDialog(@NonNull Context context) {
+            super(context);
+            this.context=context;
+        }
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            setContentView(R.layout.dialog);
+
+            camera=(TextView)findViewById(R.id.camera);
+            gallery=(TextView)findViewById(R.id.gallery);
+            cancel=(TextView)findViewById(R.id.cancel);
+
+            camera.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(cameraIntent, 1);
+                }
+            });
+
+            gallery.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(intent, 2);
+                }
+            });
+
+            cancel.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    dismiss();
+                }
+            });
+
+        }
+    }
 }
