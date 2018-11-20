608cf2164969dc30e16e99e43a6e743151a807b2
Andrew Jiang <andrewji@usc.edu>
2018-11-19 19:24:15 -0800

    initialize userlistactivity

---
+++ app/src/main/java/com/example/kindler/UserListActivity.java
@@ -0,0 +1,14 @@
+package com.example.kindler;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class UserListActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
